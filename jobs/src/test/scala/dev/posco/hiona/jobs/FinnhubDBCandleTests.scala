560d2e598d7b1f13b38a20e2b3f500cd077809fe
Tim Chklovski <tchklovski@users.noreply.github.com>
2020-06-16 18:50:22 -0700

    Tim/ci aws secrets (#81)
    * enable failing test, tweak ci config
    
    * add sbt caching, aws credentials

--- jobs/src/test/scala/dev/posco/hiona/jobs/FinnhubDBCandleTests.scala
+++ jobs/src/test/scala/dev/posco/hiona/jobs/FinnhubDBCandleTests.scala
@@ -3,8 +3,7 @@ package dev.posco.hiona.jobs
 class FinnhubDBCandleTests extends munit.FunSuite {
   test("we should be able to instantiate any Lambda") {
     // this requires AWS credentials to be found
-    // val lam = new FinnhubDBCandleLambda
-    // assert(lam != null)
-    assert(true)
+    val lam = new FinnhubDBCandleLambda
+    assert(lam != null)
   }
 }
